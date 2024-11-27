e7eb866a6223858f8b11c9a4cc27a4f7a2aaff59
bryan <bryan@PCAmairani>
2024-11-27 13:03:18 -0600

    staticfiles 2

--- ProyectoP_django/settings.py
+++ ProyectoP_django/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Si tienes archivos estáticos adicionales
+    os.path.join(BASE_DIR, 'app_1/static'),  # Si tienes archivos estáticos adicionales
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
